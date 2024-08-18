c46289bfd7e1fee713fcf476d5f679dee87ecfd1
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-08-19 00:31:48 +0500

    Add controller to get all subjects

--- api/v1/controllers/subjects.py
+++ api/v1/controllers/subjects.py
@@ -4,6 +4,7 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 
 from api.core.config import SessionLocal
+from api.models.models import Subject
 from api.services.database import store_subject_in_db
 from api.utils.request_classes import SubjectCreate
 
@@ -11,15 +12,26 @@ if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
 
-async def get_all_subjects() -> JSONResponse:
-    return JSONResponse(content={"message": "get_all_subjects"})
+async def get_all_subjects(request: Request) -> JSONResponse:  # noqa: ARG001
+    db: Session = SessionLocal()
+    subjects = db.query(Subject).all()
+    db.close()
+
+    return [
+        {
+            "id": subject.id,
+            "name": subject.name,
+            "code": subject.code
+        }
+        for subject in subjects
+    ]
 
 
 async def get_subject_by_id(subject_id: str) -> JSONResponse:
     return JSONResponse(content={"message": f"get_subject_by_id for {subject_id}"})
 
 
-async def add_subject(request: Request, subject_create: SubjectCreate) -> JSONResponse:
+async def add_subject(request: Request, subject_create: SubjectCreate) -> JSONResponse:  # noqa: ARG001
     db: Session = SessionLocal()
     subject = store_subject_in_db(db, subject_create.dict())
     db.close()
